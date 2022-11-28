3f4ba7a4b8820d7a76571bbf875d7f90bb4bcc0b
Danil Tankov <danil.tankoff@yandex.ru>
2022-11-29 01:59:00 +0700

    chore: typings for f*cking meteor libraries

--- lab-3/common/entities/accounts.ts
+++ lab-3/common/entities/accounts.ts
@@ -1,7 +1,9 @@
+import { Meteor } from 'meteor/meteor'
 import { Mongo } from 'meteor/mongo'
-import type { UserProfile } from 'meteor/meteor'
 
 import type { Tariff } from '/common/entities/tariffs'
+
+import UserProfile = Meteor.UserProfile
 
 export interface Account {
   _id?: string
@@ -11,3 +13,11 @@ export interface Account {
 }
 
 export const AccountsCollection = new Mongo.Collection<Account>('accounts')
+
+AccountsCollection.addLinks({
+  list: {
+    type: 'one',
+    field: 'listId',
+    collection: Meteor.users
+  }
+})
